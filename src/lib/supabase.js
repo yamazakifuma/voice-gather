72997ec762d42aecc3226eff62eb353b5432c79e
yamazakifuma <fuma0512@icloud.com>
2026-05-01 02:25:15 +0900

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!url || !key) {
+  console.error("Supabase の環境変数が設定されていません");
+}
+
+export const supabase = createClient(url, key);
